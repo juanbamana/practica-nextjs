4ca317767215c8a4d50c18302bd284bcf90ac9ac
juanbamana <juanmana0402@gmail.com>
2023-05-11 13:45:22 +0200

    testing css and scss

---
+++ app/CssExterno.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CssExterno.css';
+
+export const CssExterno = () => {
+  return (
+    <h1 className='css-externo'>CssExterno</h1>
+  )
+}
---
+++ app/ScssExterno.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './ScssExterno.scss'
+export const ScssExterno = () => {
+  return (
+    <h1 className='scss-externo'>ScssExterno</h1>
+  )
+}
